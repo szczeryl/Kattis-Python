2cbfc8bbb44fd74ccfd092bf45013f24ce437db3
szczeryl <qwerty844448@gmail.com>
2024-08-01 15:36:56 +0800

    Delete asciikassi.py

--- asciikassi.py
+++
@@ -1,9 +0,0 @@
-input1 = int(input())
-if input1 == 0: 
-    print("++")
-    print("++")
-else: 
-    print("+" + "-"*input1 + "+")
-    for i in range (input1):
-        print ("|" + " "* input1 + "|")
-    print("+" + "-"*input1 + "+")
